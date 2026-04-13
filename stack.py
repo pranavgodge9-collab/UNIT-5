6734a8b584cb082e8302629a579e3a2098d2b04d
pranavgodge9-collab <pranavgodge9@gmail.com>
2026-04-13 15:08:10 +0530

    Add files via upload

---
+++ stack.py
@@ -0,0 +1,46 @@
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self,item):
+        self.stack.append(item)
+        print(f"{item} is pushed to stack.")
+
+    def safe_pop(self):
+        if len(self.stack) == 0:
+            print("Stack is empty, nothing to pop.")
+            return None
+        else:
+            return self.stack.pop()
+
+    def display(self):
+        print("Stack:", self.stack)
+
+s = Stack()
+
+s.push(10)
+s.push(20)
+s.push(30)
+
+s.display()
+
+print("Popped:", s.safe_pop())
+print("Popped:", s.safe_pop())
+print("Popped:", s.safe_pop())
+
+print("Popped:", s.safe_pop())
+
+
+
+
+
+        
+
+        
+
+        
+        
+
+    
+
